6b2ea9c28bb9148171a46cbc7867d4cf8f82a283
cliff <clifflee1022@gmail.com>
2023-04-28 16:26:47 +0800

    feat: home page add css and link to post detail

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Layout, { siteTitle } from "../components/layout"
 // import Link from 'next/link'
 import utilStyles from "../styles/utils.module.css"
 import { getSortedPostsData } from "../lib/posts"
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   console.log("---getStaticProps")
@@ -36,11 +38,14 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              title: {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              id :{id}
+              {/* id :{id} */}
               <br />
-              date: {date}
+              {/* date: {date} */}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
             </li>
           ))}
         </ul>
